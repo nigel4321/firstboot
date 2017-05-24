0a95e923346cc3b4e567913e129716c7b332f116
Nigel Samaroo <nsa20@ovp.bskyb.com>
2017-05-24 22:34:22 +0100

    add compose boots

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+yum -y update
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum -y install epel-release
+yum -y install python-pip
+pip install --upgrade pip'
+
 cd /tmp
 git clone https://github.com/vegasbrianc/prometheus.git
+cd prometheus
+docker-compose up -d
