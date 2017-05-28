f78184848bb1c81c838ab87f6506bd6ac9d3c8a7
Nigel Samaroo <nsa20@ovp.bskyb.com>
2017-05-28 22:27:11 +0100

    update docker

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,10 @@ yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum -y install epel-release
 yum -y install python-pip
-yum -y install git
+yum -y install docker
 
-pip install --upgrade pip'
+pip install --upgrade pip
+pip install docker-compose
 
 systemctl enable docker
 systemctl start docker
